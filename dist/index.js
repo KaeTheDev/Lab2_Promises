5315f9c2765a2a8ee9308f861b55775d33944ebb
KaeTheDev <kaethedev@gmail.com>
2025-11-18 15:27:34 -0500

    Compiled TypeScript into JavaScript and tested fetch for Product Reviews

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,14 @@
 import { fetchProductCatalog } from "./apiSimulator.js";
+import { fetchProductReviews } from "./apiSimulator.js";
 // Fetch All Products
 fetchProductCatalog()
     .then(products => console.log("Products:", products))
+    .catch(err => console.error("Error:", err));
+// Reviews for Product 1
+fetchProductReviews(1)
+    .then(reviews => {
+    const review = reviews.map(r => r.productReview);
+    console.log("Reviews:", review);
+})
     .catch(err => console.error("Error:", err));
 //# sourceMappingURL=index.js.map
