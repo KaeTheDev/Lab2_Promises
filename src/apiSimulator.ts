a212c07cb2d107c75284bb059c62da1dbe06fbd2
KaeTheDev <kaethedev@gmail.com>
2025-11-18 15:21:13 -0500

    Added interfaces for Product and ProductReviews

---
+++ src/apiSimulator.ts
@@ -0,0 +1,11 @@
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+  }
+  
+  interface ProductReviews {
+    productId: number;
+    rating: number;
+    productReview: string;
+  }  
