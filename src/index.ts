fb91107312699eafd1919dae9ca44e7deca355c5
KaeTheDev <kaethedev@gmail.com>
2025-11-18 15:24:29 -0500

    Added fetch for all products in index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { fetchProductCatalog } from "./apiSimulator.js";
+
+// Fetch All Products
+fetchProductCatalog()
+  .then(products => console.log("Products:", products))
+  .catch(err => console.error("Error:", err));
