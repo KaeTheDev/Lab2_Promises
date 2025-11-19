8b05183f94eac5975dc5cf9f5d0255dc04224bd0
KaeTheDev <kaethedev@gmail.com>
2025-11-19 14:59:09 -0500

    Compiled typescript

--- dist/index.js
+++ dist/index.js
@@ -3,26 +3,26 @@ import { fetchProductReviews } from "./apiSimulator.js";
 import { fetchSalesReport } from "./apiSimulator.js";
 // Fetch All Products
 fetchProductCatalog()
-    .then(products => console.log("Products:", products))
-    .catch(err => console.error("Error:", err));
+    .then((products) => console.log("Products:", products))
+    .catch((err) => console.error("Error:", err));
 // Reviews for Product 1
 fetchProductReviews(1)
-    .then(reviews => {
-    const review = reviews.map(r => r.productReview);
+    .then((reviews) => {
+    const review = reviews.map((r) => r.productReview);
     console.log("Reviews:", review);
 })
-    .catch(err => console.error("Error:", err));
+    .catch((err) => console.error("Error:", err));
 // Reviews for Product 2
 fetchProductReviews(2)
-    .then(reviews => {
-    const review = reviews.map(r => r.productReview);
+    .then((reviews) => {
+    const review = reviews.map((r) => r.productReview);
     console.log("Reviews:", review);
 })
-    .catch(err => console.error("Error:", err));
+    .catch((err) => console.error("Error:", err));
 // Get all Sales Report
 fetchSalesReport()
-    .then(sales => {
+    .then((sales) => {
     console.log("Sales Report: ", sales);
 })
-    .catch(err => console.error("Error: ", err));
+    .catch((err) => console.error("Error: ", err));
 //# sourceMappingURL=index.js.map
